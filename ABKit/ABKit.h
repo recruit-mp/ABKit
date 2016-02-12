a13d90dd801fe9fb961ec2ee73bd5fdf8f10bce3
Naoto Kaneko <n_kaneko@r.recruit.co.jp>
2016-02-12 14:30:26 +0900

    Initial commit

---
+++ ABKit/ABKit.h
@@ -0,0 +1,19 @@
+//
+//  ABKit.h
+//  ABKit
+//
+//  Created by Naoto Kaneko on 2/12/16.
+//  Copyright © 2016 Recruit Marketing Partners Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ABKit.
+FOUNDATION_EXPORT double ABKitVersionNumber;
+
+//! Project version string for ABKit.
+FOUNDATION_EXPORT const unsigned char ABKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ABKit/PublicHeader.h>
+
+
---
+++ ABKitTests/ABKitTests.swift
@@ -0,0 +1,36 @@
+//
+//  ABKitTests.swift
+//  ABKitTests
+//
+//  Created by Naoto Kaneko on 2/12/16.
+//  Copyright © 2016 Recruit Marketing Partners Co.,Ltd. All rights reserved.
+//
+
+import XCTest
+@testable import ABKit
+
+class ABKitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
